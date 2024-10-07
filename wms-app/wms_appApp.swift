ebc40e98027e1c8bcd34690bef81f49a9bd193fb
microboat <13955645241@163.com>
2024-10-07 22:57:57 +0800

    Initial Commit

---
+++ wms-app/wms_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  wms_appApp.swift
+//  wms-app
+//
+//  Created by 周伟 on 2024/10/7.
+//
+
+import SwiftUI
+
+@main
+struct wms_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wms-appTests/wms_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  wms_appTests.swift
+//  wms-appTests
+//
+//  Created by 周伟 on 2024/10/7.
+//
+
+import Testing
+
+struct wms_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
